f41778278b3ac22b85cc6850bd2f6ce81222c66b
joschrew <jonas.schrewe@gwdg.de>
2024-02-07 16:01:38 +0100

    Make docker-compose creation script configurable
    So it can be used for different dest files more easily

--- create_docker_compose/create-workflow-server-docker.py
+++ create_docker_compose/create-workflow-server-docker.py
@@ -9,25 +9,35 @@ import requests
 from pathlib import Path
 import yaml
 import re
+import sys
 
-DEST = "../docker-compose.yaml"
 DEST_ENV = "../.env"
-# Purpose is to change the image of containers for debugging. Left here as reminder if needed again
+# Purpose was to change the image of containers for debugging. Left here as reminder if needed again
 IMAGE_NAME = ""
 
+CONFIG_PATH = "configs/default.yaml" if len(sys.argv) == 1 else sys.argv[1]
+assert Path(CONFIG_PATH).exists(), f"specified config file not existing: '{sys.argv[1]}'"
 
-DC_BASE_TEMPLATE = "docker-compose.template.yaml"
-SERVICE_TEMPLATE = "docker-compose.processor.template.yaml"
+with open(CONFIG_PATH) as fin:
+    config = yaml.safe_load(fin)
 
-PROCESSORS_CUSTOM = "use_processors.yaml"
-
-USE_CUSTOM_LOGGING_CONF = True
+assert isinstance(config, dict), (
+    "error reading config-file to dict. Expecting dict but is "
+    f"{type(config)}"
+)
+DEST = config["dest"]
+BASE_TEMPLATE = config["dc_base_template"]
+SERVICE_TEMPLATE = config["dc_service_template"]
+USE_CUSTOM_LOGGING_CONF = config.get("use_custom_logging_config", False)
+TESSERACT_VOL_REPLACEMENT = config.get("tesseract_vol_replacement", None)
+TESSDATA_PREFIX = config.get("tessdata_prefix", None)
 
 # processors from ocrd-all-tool-json to be skipped
 # they are present in the ocrd-tool-json but matching binaries are not available in ocrd_all. Maybe
 # they are misspelled in the ocrd-all-tool.json or no longer available in ocrd_all
 BLOCK_LIST = [
     "ocrd-cor-asv-fst-process",
+    "ocrd-dummy",
     "ocrd-pc-segmentation",
     "ocrd-ocropy-segment",
     "ocrd-page2tei",
@@ -35,15 +45,8 @@ BLOCK_LIST = [
     "ocrd-cis-ocropy-rec",
 ]
 
-FRAKTUR_VOL_REPLACEMENT = (
-    '"$PWD/Fraktur.traineddata:/models/ocrd-tesserocr-recognize/Fraktur.traineddata"'
-)
-
-# list read from PROCESSORS_CUSTOM to only create specific processors and not all
-ALLOW_LIST = []
-if Path(PROCESSORS_CUSTOM).exists():
-    with open(PROCESSORS_CUSTOM) as fin:
-        ALLOW_LIST = yaml.safe_load(fin)
+# If at least one processor specified, don't create all workers just these ones
+ALLOW_LIST = config["processors"]
 
 
 def write_docker_compose(dirname, data):
@@ -57,15 +60,19 @@ def get_processors():
     r = requests.get("https://ocr-d.de/js/ocrd-all-tool.json")
     processors = r.json().keys()
     if len(ALLOW_LIST) > 0:
-        assert all(used_proc in processors for used_proc in ALLOW_LIST), \
-            f"Processor-name in {PROCESSORS_CUSTOM} not valid"
+        assert all(used_proc in processors for used_proc in ALLOW_LIST), (
+            f"Processor-name in config (key: 'processors') not valid. "
+            f"Invalid: '{[p for p in ALLOW_LIST if not p in processors]}'"
+        )
         return ALLOW_LIST
     else:
-        return [x for x in processors if x not in BLOCK_LIST]
+        res = [x for x in processors if x not in BLOCK_LIST]
+        res.sort()
+        return res
 
 
 def dc_head() -> str:
-    with open(DC_BASE_TEMPLATE, "r") as fin:
+    with open(BASE_TEMPLATE, "r") as fin:
         return fin.read()
 
 
@@ -81,10 +88,11 @@ def dc_workers() -> str:
         template_for_processor = re.sub(r"{{[\s]*processor_name[\s]*}}", p, template)
 
         # add Fraktur models for tesseract recognize
-        if p == "ocrd-tesserocr-recognize":
+        if TESSERACT_VOL_REPLACEMENT \
+                and p in ["ocrd-tesserocr-recognize", "ocrd-tesserocr-segment-region"]:
             template_for_processor = re.sub(
                 r"    volumes:",
-                f"    volumes:\n      - {FRAKTUR_VOL_REPLACEMENT}",
+                f"    {TESSERACT_VOL_REPLACEMENT}",
                 template_for_processor,
             )
 
@@ -95,19 +103,19 @@ def dc_workers() -> str:
             )
 
         # optionally set custom logging conf through volume mount
-        if USE_CUSTOM_LOGGING_CONF:
+        if USE_CUSTOM_LOGGING_CONF and p.find("tesser") > -1:
             assert Path("../my_ocrd_logging.conf").exists(), "custom logging conf not found"
             template_for_processor = re.sub(
                 r"    volumes:",
-                "    volumes:\n      - \"./my_ocrd_logging.conf:/etc/ocrd_logging.conf\"",
+                '    volumes:\n      - "./my_ocrd_logging.conf:/root/ocrd_logging.conf"',
                 template_for_processor,
             )
 
         # optionally add environment Variable for tesseract Images:
-        if p.startswith("ocrd-tesserocr-"):
+        if p.startswith("ocrd-tesserocr-") and TESSDATA_PREFIX:
             template_for_processor = re.sub(
                 r"    environment:",
-                "    environment:\n      - TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata",
+                f"    environment:\n      - TESSDATA_PREFIX={TESSDATA_PREFIX}",
                 template_for_processor,
             )
 
